b76fdb104303d2cc32930e7db5ca2124dc60d2d7
leviajulianna09 <168497701+leviajulianna09@users.noreply.github.com>
2025-06-04 12:22:15 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -3,6 +3,9 @@ addLayer("p", {
         symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized
         position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
         color: "#000000",
+	nodeStyle() { return {
+			color: (player.oldStyle?"white":"rgba(255, 255, 255, 0.75)"),
+		}},
         requires: new Decimal(10), // Can be a function that takes requirement increases into account
         resource: "rizz", // Name of prestige currency
         baseResource: "aura", // Name of resource prestige is based on
